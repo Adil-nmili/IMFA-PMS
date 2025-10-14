fbe0e7e9cd1ccd7e1a6e4ce47e2cf96954b01963
SAAD-ELGHABA <saadlraba@gmail.com>
2025-10-14 17:29:49 +0100

    supabase config

---
+++ src/pages/loginPage.tsx
@@ -0,0 +1,11 @@
+
+function Login() {
+  return (
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2">
+      <div></div>
+      <div className="bg-[#967E62]"></div>
+    </div>
+  )
+}
+
+export default Login
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-
+import Login from '@/pages/loginPage'
 export const HOME = "/";
-
+export const LOGIN = "/login"
 
 const Router = createBrowserRouter([
     {
         path: HOME,
         element: <div>Home Page</div>,
     },
+    {
+        path: LOGIN,
+        element: <Login/>,
+    }
   
 ]);
 
