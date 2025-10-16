5570c607024bc4577e5f7bbf89842ebd4a19a299
FerdawsAnzer <22301240@student.ciu.edu.tr>
2025-10-16 10:51:07 +0300

    create sidebar and header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Button } from "./components/ui/button"
-
-
+import SideBar from "./layoutComponent/sidebar"
+import Header from "./layoutComponent/header"
 function App() {
-
-  return (
-  <div >
-          <Button>add </Button>
+return (
+  <div className="flex h-screen">
+    <aside>
+      <SideBar/>
+    </aside>
+    <div className="flex-1 flex flex-col">
+        <main className="ml-4"> 
+            <Header />
+         </main>
+          {/* Page content */}
+        <div className="p-6">
+            Page Content
+        </div>
+      </div>
   </div>
-  )
+  );
 }
+
+
 
 export default App
