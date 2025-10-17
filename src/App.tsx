bf5fe247894112d52d46bc787da9475ed3afc7d2
FerdawsAnzer <22301240@student.ciu.edu.tr>
2025-10-17 13:49:53 +0300

    updated sidebar hover and routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
+import { Routes, Route } from "react-router-dom";
 import SideBar from "./layoutComponent/sidebar"
 import Header from "./layoutComponent/header"
+import Dashboard from "./pages/Dashboard";
+import ReservationPage from "./pages/reservation";
+
 function App() {
 return (
-  <div className="flex h-screen">
+<div className="flex h-screen">
     <aside>
       <SideBar/>
     </aside>
-    <div className="flex-1 flex flex-col">
-        <main className="ml-4"> 
+  <div className="flex-1 flex flex-col">
+    <main className="ml-4"> 
             <Header />
-         </main>
-          {/* Page content */}
-        <div className="p-6">
-            Page Content
-        </div>
-      </div>
+        {/* Page content */}  
+       <div className="p-6">
+           <Routes>
+              <Route path="/" element={<Dashboard />} />
+               <Route path="/reservation" element={<ReservationPage />} />
+            </Routes>
+          </div> 
+        </main> 
+    </div>
   </div>
   );
 }
--- src/layoutComponent/sidebar.tsx
+++ src/layoutComponent/sidebar.tsx
@@ -3,32 +3,35 @@ import { FaBed,FaSignOutAlt,FaWallet,FaCog,FaCheckCircle,} from "react-icons/fa"
 import { HiMenuAlt2 } from "react-icons/hi";
 import { BsCalendarCheck } from "react-icons/bs";
 import Logo from "../assets/hotelLogo.png";
+import { Link } from "react-router-dom";
 
 interface MenuItem {
   icon: React.ComponentType<{ className?: string }>;
   name: string;
+  path?:string;
 }
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
+ 
   const topMenuItems: MenuItem[] = [
-    { icon: HiMenuAlt2, name: "Dashboard" },
-    { icon: BsCalendarCheck, name: "Reservation" },
-    { icon: FaBed, name: "Rooms" },
-    { icon: FaCheckCircle, name: "Checks" },
-    { icon: FaWallet, name: "Billing" },
+    { icon: HiMenuAlt2, name: "Dashboard",path:"/" },
+    { icon: BsCalendarCheck, name: "Reservation" ,path:"/reservation"},
+    { icon: FaBed, name: "Rooms" ,path:""},
+    { icon: FaCheckCircle, name: "Checks",path:"" },
+    { icon: FaWallet, name: "Billing",path:"" },
   ];
 
   const bottomMenuItems: MenuItem[] = [
-    { icon: FaCog, name: "Settings" },
-    { icon: FaSignOutAlt, name: "Logout" },
+    { icon: FaCog, name: "Settings",path:"" },
+    { icon: FaSignOutAlt, name: "Logout",path:"" },
   ];
 
   return (
 <div className="pl-6 pt-4.5"> 
     <div
       className={`flex flex-col h-[95vh] bg-[#3b2f2f] text-gray-100 p-3 rounded-2xl font-roboto transition-all duration-300 overflow-hidden ${isOpen ? "w-[200px]" : "w-[70px]"
-        }`}
+        }`} onMouseEnter={() => setIsOpen(true)}
+    onMouseLeave={() => setIsOpen(false)}
      >{/* Logo */}
     <li className="flex items-center justify-start mb-4">
         <div className="flex justify-start overflow-hidden w-[80px]">
@@ -43,11 +46,10 @@ const SideBar = () => {
       </li>
       {/*Top Menu*/}
     <ul className="flex flex-col">
-        {topMenuItems.map(({ icon: Icon, name }, index) => (
-        <li
-            key={index}
-            title={name}
-            onClick={() => index === 0 && setIsOpen(!isOpen)}
+        {topMenuItems.map(({ icon: Icon, name,path }, index) => (
+       <li key={index}title={name}>
+         <Link to={path || "#"}
+         
             className="flex items-center justify-start gap-2 h-[50px] px-3 text-lg hover:bg-[#E9E6E1] hover:text-[#3b2f2f] rounded-md cursor-pointer transition-all duration-300 ml-0.5"
         >
             <Icon className={`${
@@ -55,22 +57,23 @@ const SideBar = () => {
               }
               transition-all duration-300`}/>
             {isOpen && <span className="ml-2 text-[16px] whitespace-nowrap">{name}</span>}
+            </Link>
           </li>
         ))}
     </ul>
       {/* Bottom Menu */}
       <ul className="flex flex-col mt-auto gap-4 pb-5">
-        {bottomMenuItems.map(({ icon: Icon, name }, index) => (
-          <li
-            key={index}
-            title={name}
+        {bottomMenuItems.map(({ icon: Icon, name,path }, index) => (
+          <li key={index}
+            title={name}>
+            <Link to ={path || "#"}
             className="flex items-center justify-start gap-2 h-[50px] px-3 text-lg hover:bg-[#E9E6E1] hover:text-[#3b2f2f] rounded-md cursor-pointer transition-all duration-300"
           >
           <Icon className={`${
                 isOpen ? "text-2xl" : "text-1xl"
               }
               transition-all duration-300`}/>
-            {isOpen && <span className="ml-2 text-[16px] whitespace-nowrap">{name}</span>}
+            {isOpen && <span className="ml-2 text-[16px] whitespace-nowrap">{name}</span>}</Link>
           </li>
         ))}
     </ul>
