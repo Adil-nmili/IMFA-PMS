f31bc6a6d9abb747e8e0319907312aeca1b2599a
Adil Nmili <128920526+Adil-nmili@users.noreply.github.com>
2025-11-01 10:46:54 +0000

    add some changes

--- src/components/partials/sidebar.tsx
+++ src/components/partials/sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Logo from "../../assets/hotelLogo.png";
 import {Bed, LogOut,Wallet, Cog, CheckCircle, MenuSquare,CalendarCheck} from 'lucide-react'
 import { Link } from "react-router-dom";
-import { ACCUEIL, RESERVATIONS } from "@/router/router";
+import { ACCUEIL, RESERVATIONS, ROOMS } from "@/router/router";
 
 interface MenuItem {
   icon: React.ComponentType<{ className?: string }>;
@@ -15,7 +15,7 @@ const SideBar = () => {
   const topMenuItems: MenuItem[] = [
     { icon: MenuSquare, name: "Dashboard",path:ACCUEIL },
     { icon: CalendarCheck, name: "Reservation" ,path:RESERVATIONS},
-    { icon: Bed, name: "Rooms" ,path:""},
+    { icon: Bed, name: "Rooms" ,path:ROOMS},
     { icon: CheckCircle, name: "Checks",path:"" },
     { icon: Wallet, name: "Billing",path:"" },
   ];
